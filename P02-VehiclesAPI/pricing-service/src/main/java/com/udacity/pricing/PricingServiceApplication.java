3c5ebf177405729a33b86f30477acda605551085
richard lu <luxh93@gmail.com>
2021-03-22 14:10:10 -0400

    read from price json

--- P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
+++ P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
@@ -2,10 +2,13 @@ package com.udacity.pricing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.repository.init.Jackson2RepositoryPopulatorFactoryBean;
 
 /**
  * Creates a Spring Boot Application to run the Pricing Service.
- * TODO: Convert the application from a REST API to a microservice.
+ * Convert the application from a REST API to a microservice.
  */
 @SpringBootApplication
 public class PricingServiceApplication {
@@ -14,4 +17,10 @@ public class PricingServiceApplication {
         SpringApplication.run(PricingServiceApplication.class, args);
     }
 
+    @Bean
+    public Jackson2RepositoryPopulatorFactoryBean getRepositoryPopulator() {
+        Jackson2RepositoryPopulatorFactoryBean factory = new Jackson2RepositoryPopulatorFactoryBean();
+        factory.setResources(new ClassPathResource[] { new ClassPathResource("prices.json") });
+        return factory;
+    }
 }
--- P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,16 +1,28 @@
 package com.udacity.pricing;
 
+import com.udacity.pricing.domain.price.Price;
+import com.udacity.pricing.domain.price.PriceRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PricingServiceApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+	@Autowired
+	PriceRepository priceRepository;
 
+	@Test
+	public void givenPriceJsonPopulatorThenShouldInsertPriceOnStart() {
+		List<Price> fruits = priceRepository.findAll();
+		assertEquals("record count is not matching", 5, fruits.size());
+	}
 }
