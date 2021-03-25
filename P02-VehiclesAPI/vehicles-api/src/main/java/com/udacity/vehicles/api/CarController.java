b5d459f114270e7007ed916c9c5361f7dc939699
richard lu <luxh93@gmail.com>
2021-03-25 11:37:44 -0400

    CRUD operations for vehicle api

--- P02-VehiclesAPI/vehicles-api/src/main/java/com/udacity/vehicles/api/CarController.java
+++ P02-VehiclesAPI/vehicles-api/src/main/java/com/udacity/vehicles/api/CarController.java
@@ -52,21 +52,22 @@ class CarController {
     }
 
     /**
+     * Task: The Vehicles API can receive GET requests from a user, and read back
+     * either a list of all existing vehicles, or the data for a single vehicle.
+     *
      * Gets information of a specific car by ID.
      * @param id the id number of the given vehicle
      * @return all information for the requested vehicle
      */
     @GetMapping("/{id}")
     Resource<Car> get(@PathVariable Long id) {
-        /**
-         * TODO: Use the `findById` method from the Car Service to get car information.
-         * TODO: Use the `assembler` on that car and return the resulting output.
-         *   Update the first line as part of the above implementing.
-         */
-        return assembler.toResource(new Car());
+        Car car = carService.findById(id);
+        return assembler.toResource(car);
     }
 
     /**
+     * TASK: The Vehicles API is able to create a new vehicle based on input from the user with a POST request.
+     *
      * Posts information to create a new vehicle in the system.
      * @param car A new vehicle to add to the system.
      * @return response that the new vehicle was added to the system
@@ -74,16 +75,14 @@ class CarController {
      */
     @PostMapping
     ResponseEntity<?> post(@Valid @RequestBody Car car) throws URISyntaxException {
-        /**
-         * TODO: Use the `save` method from the Car Service to save the input car.
-         * TODO: Use the `assembler` on that saved car and return as part of the response.
-         *   Update the first line as part of the above implementing.
-         */
-        Resource<Car> resource = assembler.toResource(new Car());
+        Car newCar = carService.save(car);
+        Resource<Car> resource = assembler.toResource(newCar);
         return ResponseEntity.created(new URI(resource.getId().expand().getHref())).body(resource);
     }
 
     /**
+     * TASK: The Vehicles API can update an existing vehicle through input from the user.
+     *
      * Updates the information of a vehicle in the system.
      * @param id The ID number for which to update vehicle information.
      * @param car The updated information about the related vehicle.
@@ -91,26 +90,22 @@ class CarController {
      */
     @PutMapping("/{id}")
     ResponseEntity<?> put(@PathVariable Long id, @Valid @RequestBody Car car) {
-        /**
-         * TODO: Set the id of the input car object to the `id` input.
-         * TODO: Save the car using the `save` method from the Car service
-         * TODO: Use the `assembler` on that updated car and return as part of the response.
-         *   Update the first line as part of the above implementing.
-         */
-        Resource<Car> resource = assembler.toResource(new Car());
+        car.setId(id);
+        Car newCar = carService.save(car);
+        Resource<Car> resource = assembler.toResource(newCar);
         return ResponseEntity.ok(resource);
     }
 
     /**
+     * TASK: The Vehicles API can delete an existing vehicle when requested by the user.
+     *
      * Removes a vehicle from the system.
      * @param id The ID number of the vehicle to remove.
      * @return response that the related vehicle is no longer in the system
      */
     @DeleteMapping("/{id}")
     ResponseEntity<?> delete(@PathVariable Long id) {
-        /**
-         * TODO: Use the Car Service to delete the requested vehicle.
-         */
+        carService.delete(id);
         return ResponseEntity.noContent().build();
     }
 }
